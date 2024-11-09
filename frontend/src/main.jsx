ac8adbe18f4cb05f3f789df1337da18e8211ebb3
Daniel Rodrigues <daniel.rodrigues46@fatec.sp.gov.br>
2024-11-09 02:33:11 +0000

    backend

--- frontend/src/main.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import Rotas from './rotas';
-// import { AuthProvider } from './context/AuthContext';
-import './styles/global.css';
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-      <Rotas />
-);
