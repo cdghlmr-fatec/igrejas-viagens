0317fce9c77b96364262f55fc264d5276b79dbe0
Daniel Rodrigues <daniel.rodrigues46@fatec.sp.gov.br>
2024-11-26 23:20:51 -0300

    backend

--- src/main/java/com/fatec/api/models/Reservation.java
+++ src/main/java/com/fatec/api/models/Reservation.java
@@ -1,9 +1,5 @@
 package com.fatec.api.models;
-
-import java.util.HashSet;
 import java.util.List;
-import java.util.Set;
-import org.springframework.data.mongodb.core.mapping.DBRef;
 
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
